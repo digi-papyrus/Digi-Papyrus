0aad3743d30a32d0a26fa6a81ae644eac87e15c9
erwinschrodinger1 <sunnynirjal@gmail.com>
2022-04-16 09:03:20 +0545

    for build

--- server/routes/get.js
+++ server/routes/get.js
@@ -1,57 +1,34 @@
-const express= require("express");
-const router= express.Router();
-const dotenv= require("dotenv");
-const User= require("../models/userSchema");
-const cookieParser= require("cookie-parser");
-const auth= require("../middleware/auth");
-const jwt= require("jsonwebtoken");
+const express = require("express");
+const router = express.Router();
+const dotenv = require("dotenv");
+const User = require("../models/userSchema");
+const cookieParser = require("cookie-parser");
+const auth = require("../middleware/auth");
+const jwt = require("jsonwebtoken");
 
 router.use(cookieParser());
-dotenv.config({path:"../config.env"});
-<<<<<<< HEAD
-router.post("/authenticate",(req,res)=>
-=======
-router.post("/authenticate",async (req,res)=>
->>>>>>> origin/mahip
-{ 
+dotenv.config({ path: "../config.env" });
+router.post("/authenticate", async (req, res) => {
+  try {
+    const token = req.body.token;
+    console.log(token);
 
-    try {
-    
-        const token= req.body.token;
-<<<<<<< HEAD
-=======
-        console.log(token);
+    const sec = process.env.SECRET_KEY;
+    const verifyUser = jwt.verify(token, sec);
 
->>>>>>> origin/mahip
-        
+    const user = await User.findOne({
+      _id: verifyUser._id,
+      "tokens.token": token,
+    });
 
-        const sec=process.env.SECRET_KEY;
-        const verifyUser= jwt.verify(token,sec);
-       
-        
-<<<<<<< HEAD
-        const user=User.findOne({_id:verifyUser._id,"tokens.token":token});
-        
-        
-=======
-        const user= await User.findOne({_id:verifyUser._id,"tokens.token":token});
-        
-        console.log(user);
->>>>>>> origin/mahip
-        if(!user){
-            
-            throw error;
-        }
-    
-        
-    } catch (error) {
-        console.log("ERROR FOUND");
-<<<<<<< HEAD
-        res.status(202).json("");
-=======
-        res.status(202).send("");
->>>>>>> origin/mahip
+    console.log(user);
+    if (!user) {
+      throw error;
     }
+  } catch (error) {
+    console.log("ERROR FOUND");
+    res.status(202).send("");
+  }
 });
 
-module.exports=router;
+module.exports = router;
